57589aeb1caf085b7557263894e8a76d63011838
jfjfjbjb <697451yr@163.com>
2026-06-13 22:25:55 +0800

    fix: cesium 手机白屏

--- cesium/src/App.vue
+++ cesium/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { onMounted, ref, reactive } from "vue";
 import * as Cesium from "cesium";
+// import VConsole from "vconsole";
 import useEntity from "./composables/useEntity";
 
+// const vConsole = new VConsole();
+
 Cesium.Ion.defaultAccessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MjEyM2MwOC1hNzZjLTRkMjQtODg3NS1iNzk2M2E4Yjk4Y2YiLCJpZCI6NDQ0MDE3LCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoidW5kZWZpbmVkX2RlZmF1bHQiLCJpYXQiOjE3ODEzNTczMzd9.49nPakKMpDRLllEi_w9uw2eUpsn1UUv5TyoeCzlYB04";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzMTY0M2ZkNC03M2UwLTRhMjAtYmYxZS00N2EwYzc4M2EyODAiLCJpZCI6NDQ0MDE3LCJzdWIiOiJqZmpmamJqYiIsImlzcyI6Imh0dHBzOi8vYXBpLmNlc2l1bS5jb20iLCJhdWQiOiJnaXRodWIiLCJpYXQiOjE3ODEzNTk4MTh9.rDoIXDPEuB14RoxL4GADRGfda8TwnxfwFCuXlV_9x8k";
 
 const container = ref(null);
 const showPanel = ref(false);
--- cesium/vite.config.js
+++ cesium/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cesium from 'vite-plugin-cesium'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   base: './',
-  plugins: [vue(), cesium()],
+  plugins: [vue(), cesium(), basicSsl()],
+  server: {
+    host: '0.0.0.0',
+    https: true,
+  }
 })
